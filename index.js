8b1fde503e6afb0e7ec9657d99b5542f057ca5c6
kanweitech <kanweiedward@gmail.com>
2025-03-10 09:21:54 -0700

    mongodb added!

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
-const express = require('express')
+const express = require('express');
 
-const app = express()
+const app = express();
 
-const port = process.env.PORT || 5000
+const mongoose = require('mongoose');
 
-function main() {
+const port = process.env.PORT || 5000;
+require("dotenv").config();
+
+// middleware
+app.use(express.json());
+
+async function main() {
+    await mongoose.connect(process.env.DB_URL);
     app.use("/", (req, res) => {
         res.send("Book Store Server is running!");
     });
 }
 
-main();
+main().then(() => console.log("Mongodb connected successfully!")).catch(err => console.log(err));
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
